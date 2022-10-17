7643cc1c937c2a43e5bdb13163747a15ccd87205
Rodrigo Mosken <rodrigo.mosken@outlook.com>
2022-10-17 19:08:25 -0300

    Add files via upload

---
+++ src/main/java/br/com/itau/grupo4/ticketsmicroservice/TicketsmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.itau.grupo4.ticketsmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketsmicroserviceApplication.class, args);
+	}
+
+}
