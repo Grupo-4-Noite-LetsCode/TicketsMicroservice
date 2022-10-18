a1950065c67db18397784ee02a36e144f29455e8
romosken <rodrigo.mosken@outlook.com>
2022-10-18 12:47:33 -0300

    bug/correçcao import

--- src/main/java/br/com/itau/grupo4/ticketsmicroservice/controller/advice/ErrorHandler.java
+++ src/main/java/br/com/itau/grupo4/ticketsmicroservice/controller/advice/ErrorHandler.java
@@ -1,7 +1,6 @@
 package br.com.itau.grupo4.ticketsmicroservice.controller.advice;
 
 import br.com.itau.grupo4.ticketsmicroservice.controller.advice.formatting.ErrorResponse;
-import br.com.itau.grupo4.ticketsmicroservice.model.exception.EntityException;
 import br.com.itau.grupo4.ticketsmicroservice.model.exception.TicketNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
