1795518a0ca0c634478cf27195b056b4dcc07890
VictoriaBastos <victoria.rezbastos@gmail.com>
2022-10-18 14:22:51 -0700

    chore: adding ticket cancellation request to controller

--- src/main/java/br/com/itau/grupo4/ticketsmicroservice/controller/TicketController.java
+++ src/main/java/br/com/itau/grupo4/ticketsmicroservice/controller/TicketController.java
@@ -1,13 +1,10 @@
 package br.com.itau.grupo4.ticketsmicroservice.controller;
 
-import br.com.itau.grupo4.ticketsmicroservice.dto.TicketResponse;
+import br.com.itau.grupo4.ticketsmicroservice.dto.CanceledTicketResponse;
 import br.com.itau.grupo4.ticketsmicroservice.service.TicketService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -16,12 +13,9 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class TicketController {
 
-    private final TicketService service;
-
-    @GetMapping("{id}")
-    public ResponseEntity<TicketResponse> findTicketById(@PathVariable UUID id){
-        var response = service.findById(id);
-        return ResponseEntity.ok(response);
-
+    private final TicketService ticketService;
+    @PatchMapping("/cancelamento/{id}")
+    public ResponseEntity<CanceledTicketResponse> cancelTicket(@PathVariable("id") UUID id){
+        return ResponseEntity.ok(ticketService.cancel(id));
     }
 }
