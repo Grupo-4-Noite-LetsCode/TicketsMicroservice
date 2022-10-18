1333119bde49cd1a233348426a5cc68e9a8113a5
Ellen Cintra Monteiro <ellenc.monteiro@gmail.com>
2022-10-18 19:57:46 -0300

    ajustando import

--- src/main/java/br/com/itau/grupo4/ticketsmicroservice/exception/TicketNotFoundException.java
+++ src/main/java/br/com/itau/grupo4/ticketsmicroservice/exception/TicketNotFoundException.java
@@ -1,4 +1,4 @@
-package br.com.itau.grupo4.ticketsmicroservice.model.exception;
+package br.com.itau.grupo4.ticketsmicroservice.exception;
 
 public class TicketNotFoundException extends RuntimeException{
     public TicketNotFoundException(String message){
