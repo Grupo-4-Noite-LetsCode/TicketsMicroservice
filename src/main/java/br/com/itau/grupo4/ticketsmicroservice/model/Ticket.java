7b61f21b83691a1eed8d01e4d8501b46ded83195
Ellen Cintra Monteiro <ellenc.monteiro@gmail.com>
2022-10-19 18:29:15 -0300

    removendo import não usado

--- src/main/java/br/com/itau/grupo4/ticketsmicroservice/model/Ticket.java
+++ src/main/java/br/com/itau/grupo4/ticketsmicroservice/model/Ticket.java
@@ -2,7 +2,6 @@ package br.com.itau.grupo4.ticketsmicroservice.model;
 
 import br.com.itau.grupo4.ticketsmicroservice.enums.TicketStatus;
 import br.com.itau.grupo4.ticketsmicroservice.enums.TicketType;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Type;
